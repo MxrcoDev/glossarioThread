b178c547b08c76da50f3f95bb1ec26dc516e1793
MxrcoDev.js <87544499+MxrcoDev@users.noreply.github.com>
2025-11-02 14:47:57 +0100

    Update worker parameters and improve task handling

--- worker.cpp
+++ worker.cpp
@@ -11,9 +11,10 @@
 
 using namespace std;
 
-int N_TASK = 3;
-int N_WORKERS = 1;
-int MAX_TASK_TIME = 3;
+// ----- PARAMETRI DI CONFIGURAZIONE ----- //
+int N_TASK = 100;
+int N_WORKERS = 10;
+int MAX_TASK_TIME = 10;
 
 mutex memoryMutex, outputMutex; // Mutex per l'accesso ai dati e alla console
 
@@ -29,121 +30,183 @@ int trandom() {
 
 // ----- CLASSE TASK ----- 
 class Task {
-    public:
-        int id;
-        int tempoEsecuzione = trandom();
+public:
+    int id;
+    int tempoEsecuzione = trandom();
 
-        Task(int i) : id(i){}
+    Task(int i) : id(i) {}
 };
 
 // ----- CLASSE WORKER -----
 class Worker {
-    private: 
-        queue<Task>& codaTask;
-        int id;
-        thread workerThread;
-        atomic<bool>& stop;
-        vector<thread> taskThreads;
+private: 
+    queue<Task>& codaTask; // Coda delle task, passata per riferimento
+    int id;                // ID del worker
+    thread workerThread;   // Thread del worker, avviato nel costruttore
+    atomic<bool>& stop;    // Flag per indicare se il worker deve fermarsi
+    promise<string> prom;  // Promise per comunicare il risultato finale
 
-    public:
-        // Costruttore
-        Worker(queue<Task>& q, int id, atomic<bool>& stopFlag): codaTask(q), id(id), stop(stopFlag) {
-            workerThread = thread(&Worker::ElaboraTask, this);  // Avvio il thread del worker nel costruttore
+public:
+    // Costruttore
+    Worker(queue<Task>& q, int id, atomic<bool>& stopFlag)
+        : codaTask(q), id(id), stop(stopFlag) {
+        workerThread = thread(&Worker::ElaboraTask, this);  // Avvio il thread del worker nel costruttore
+    }
+
+    // Distruttore (Joina il thread del worker)
+    ~Worker() {
+        if (workerThread.joinable()) {
+            workerThread.join();
         }
+    }
 
-        // Distruttore (Joina il thread del worker)
-        ~Worker() {
-            if (workerThread.joinable()) {
-                workerThread.join();
-            }
-        }
+    // Restituisce il future associato a questa promise
+    future<string> getFuture() {
+        return prom.get_future();
+    }
 
-        // Metodo per elaborare i task
-        void ElaboraTask() {
-            while(true) {
+    // Metodo per elaborare i task
+    void ElaboraTask() {
+        int count = 0;              // conta quante task ha svolto
+        string resultFinale;        // accumula info finali
+        bool promiseSet = false;    // evita doppia set_value
 
-                Task task(0); // task di default
+        while (true) {
+            Task task(0);            // task temporanea
+            bool haTask = false;     // Indica se il worker ha prelevato una task
 
-                // Preleva la task dalla coda
-                {
-                    lock_guard<mutex> lock(memoryMutex);
-                    if (!codaTask.empty()) {
-                        task = codaTask.front();
-                        codaTask.pop();
-                    } else if(stop) {
-                        break;
-                    }
+            // Preleva la task dalla coda
+            {
+                lock_guard<mutex> lock(memoryMutex);
+                if (!codaTask.empty()) {
+                    task = codaTask.front();
+                    codaTask.pop();
+                    haTask = true;
+                } else if (stop) {
+                    break; // esce se non ci sono più task e stop è attivo
                 }
+            }
 
-
+            // Se ha una task, procede con l'elaborazione
+            if (haTask) {
                 {
                     lock_guard<mutex> lock(outputMutex);
                     cout << "[WORKER #" << id << "] Ricevuta task con ID: " << task.id << ", Tempo di esecuzione: " << task.tempoEsecuzione << endl;
                 }
 
-                // Creazione dei threads per l'elaborazione dei task
-                taskThreads.push_back(thread([this, task]() {
-                    this_thread::sleep_for(chrono::seconds(task.tempoEsecuzione));
+                // Simula l'elaborazione con uno sleep e stampa il messaggio di completamento
+                this_thread::sleep_for(chrono::seconds(task.tempoEsecuzione));
+                {
                     lock_guard<mutex> lock(outputMutex);
-                    cout << "[WORKER #" << id << "] Terminata task con ID: " << task.id << endl;
-                }));
-            }
+                    cout << "[WORKER #" << id << "] Task " << task.id << " completata." << endl;
+                }
 
-            // Joina tutti i thread attivi che ha creato
-            for(auto& t : taskThreads) {
-                if(t.joinable())
-                    t.join();
+                count++; // Incrementa il contatore di task svolte
+                resultFinale += to_string(task.id) + ", ";
+
+            } else {
+                // Evita busy-waiting quando la coda è vuota
+                this_thread::sleep_for(chrono::milliseconds(100));
             }
         }
+
+        // Restituisce il risultato finale
+        if (!promiseSet) {
+            string res = "Worker " + to_string(id) + " ha completato " + to_string(count) + " task. Task svolte: " + resultFinale;
+            prom.set_value(res);
+            promiseSet = true;
+        }
+
+        // Messaggio di terminazione del worker
+        {
+            lock_guard<mutex> lock(outputMutex);
+            cout << "[WORKER #" << id << "] Terminato dopo " << count << " task.\n";
+        }
+    }
+
+    // Metodo per attendere il thread del worker (usato dal Master)
+    void join() {
+        if (workerThread.joinable()) {
+            workerThread.join();
+        }
+    }
 };
 
 // ----- CLASSE MASTER -----
 class Master {
-    private:
-        queue<Task> codaTask;
-        vector<unique_ptr<Worker>> workers;
-        atomic<bool> stopFlag{false};
-    public:
-        Master() {}
+private:
+    queue<Task> codaTask;                   // Coda delle task
+    vector<unique_ptr<Worker>> workers;     // Vettore dei workers
+    atomic<bool> stopFlag{false};           // Flag per la terminazione dei worker
+    vector<future<string>> futures;         // memorizza i future dei worker
+    
+public:
 
-        void generaTasks() {
-            for(int i = 0; i < N_TASK; i++) {
-                lock_guard<mutex> lock(memoryMutex);
-                codaTask.emplace(i); // Creo la Task direttamente nella coda
-                cout << "[MASTER] Generato task con ID: " << i << ", Tempo di esecuzione: " << codaTask.back().tempoEsecuzione << endl;
-            }
+    // Costruttore (non passo alcun paramentro nel costruttore perchè non serve)
+    Master() {}
+
+    // Metodo per generare le task
+    void generaTasks() {
+        for (int i = 0; i < N_TASK; i++) {
+            lock_guard<mutex> lock(memoryMutex);
+            codaTask.emplace(i); // Creo la Task direttamente nella coda
+
+            cout << "[MASTER] Generato task con ID: " << i << ", Tempo di esecuzione: " << codaTask.back().tempoEsecuzione << endl;
         }
+    }
 
-        void generaWorkers() {
-            for(int i = 0; i < N_WORKERS; i++) {
-                workers.emplace_back(make_unique<Worker>(codaTask, i, stopFlag)); // Crea un nuovo worker e lo aggiunge al vettore
-            }
+    // Metodo per generare i workers
+    void generaWorkers() {
+        for (int i = 0; i < N_WORKERS; i++) {
+            auto worker = make_unique<Worker>(codaTask, i, stopFlag);   // Creo un worker con un puntatore unico
+            futures.push_back(worker->getFuture());                     // salvo il future del worker
+            workers.push_back(move(worker));                            // Aggiungi il worker al vettore
         }
+    }
 
-        void execute() {
-            generaTasks();
-            this_thread::sleep_for(chrono::seconds(3));
-            cout << "\n-------------------------------------------\n\n";
-            generaWorkers();
+    // Metodo principale
+    void execute() {
+        generaTasks();                                                  // Genera le task
+        this_thread::sleep_for(chrono::seconds(1));                     // Attendi
+        cout << "\n-------------------------------------------\n\n";
+        generaWorkers();                                                // Genera i workers
 
-            // Ciclo per verificare se ci sono task da elaborare, in caso contrario distruggi i workers
-            while(!stopFlag) {
+        // Ciclo per verificare se ci sono task da elaborare, in caso contrario distruggi i workers
+        while (!stopFlag) {
+            {
                 lock_guard<mutex> lock(memoryMutex);
-                if(codaTask.empty()) {
-                    cout << "[MASTER] Tutte le tasks elaborate" << endl;
+                if (codaTask.empty()) {
+                    cout << "[MASTER] Tutte le tasks prese in elaborazione" << endl;
                     stopFlag = true;
                 }
-                this_thread::sleep_for(chrono::seconds(1)); // Controlla ogni secondo per evitare busy waiting
             }
-
+            this_thread::sleep_for(chrono::seconds(1)); // Controlla ogni secondo per evitare busy waiting
         }
+    }
+
+    // Attende che tutti i worker finiscano
+    void waitWorkers() {
+        cout << "[MASTER] In attesa che tutti i worker terminino...\n";
+        for (auto& w : workers) {
+            w->join();              // Joina tutti i worker
+        }
+        cout << "[MASTER] Tutti i worker terminati.\n";
+    }
+    
+    void restituisciRisultati() {
+        // Stampa Vettore proms
+        cout << "\n========== RISULTATI ==========\n";
+        for (auto& f : futures) {
+            cout << f.get() << endl;
+        }
+        cout << "==============================\n";
+    }
 };
 
 int main() {
-
-    Master master;
-
-    master.execute();
-
+    Master master;                  // Crea un oggetto Master
+    master.execute();               // Esegue il program
+    master.waitWorkers();           // Aspetta la terminazione di tutti i worker
+    master.restituisciRisultati();  // Restituisce i risultati tramite i futures
     return 0;
 }
